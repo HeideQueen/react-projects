4aeebad0433b9a1fb102019b7fcfc36a4bd6d771
HeideQueen <lucyfabz@gmail.com>
2021-04-16 18:57:53 -0500

    color generator complete

--- 9-color-generator/setup/src/SingleColor.js
+++ 9-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import rgbToHex from './utils'
+import React, { useState, useEffect } from 'react';
+// import rgbToHex from './utils';
 
-const SingleColor = () => {
-  return <h4>single color</h4>
-}
+const SingleColor = ({ rgb, weight, index, hexColor }) => {
+  const [alert, setAlert] = useState(false);
 
-export default SingleColor
+  const bcg = rgb.join(',');
+
+  // const hex = rgbToHex(...rgb);
+
+  const hexValue = `#${hexColor}`;
+
+  useEffect(() => {
+    const alertTimeout = setTimeout(() => {
+      setAlert(false);
+    }, 2000);
+
+    return () => {
+      clearTimeout(alertTimeout);
+    };
+  }, [alert]);
+
+  return (
+    <article
+      className={`color ${index > 10 ? 'color-light' : null}`}
+      style={{ backgroundColor: `rgb(${bcg})` }}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p className='percent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+};
+
+export default SingleColor;
