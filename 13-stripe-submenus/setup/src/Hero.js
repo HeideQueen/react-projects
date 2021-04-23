b5c4de1e69c6a449d58db6ef607f86b1fe81ffa8
HeideQueen <lucyfabz@gmail.com>
2021-04-23 02:43:06 -0500

    navbar complete

--- 13-stripe-submenus/setup/src/Hero.js
+++ 13-stripe-submenus/setup/src/Hero.js
@@ -1,8 +1,29 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from 'react';
+import phoneImg from './images/phone.svg';
+import { useGlobalContext } from './context';
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+  const { closeSubmenu } = useGlobalContext();
 
-export default Hero
+  return (
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>Payment infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores
+            iure perferendis ut reiciendis ex delectus fugit blanditiis
+            repellendus? Magni rem odio alias consectetur deserunt aliquam sunt
+            aliquid veniam? Eaque, non!
+          </p>
+          <button className='btn'>Start now</button>
+        </article>
+        <article className='hero-images'>
+          <img src={phoneImg} alt='phone' className='phone-img' />
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- 13-stripe-submenus/setup/src/Sidebar.js
+++ 13-stripe-submenus/setup/src/Sidebar.js
@@ -1,9 +1,44 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import { FaTimes } from 'react-icons/fa';
+import { useGlobalContext } from './context';
+import sublinks from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+  const { closeSidebar, isSidebarOpen } = useGlobalContext();
 
-export default Sidebar
+  return (
+    <aside
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      <div className='sidebar'>
+        <button className='close-btn' onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((link, index) => {
+            const { links, page } = link;
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <article className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link;
+                    return (
+                      <a href={url} key={index}>
+                        {icon} {label}
+                      </a>
+                    );
+                  })}
+                </article>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
