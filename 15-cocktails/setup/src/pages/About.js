050babc6314e033bf5e26e3f9634cc1d6c1b41f9
HeideQueen <lucyfabz@gmail.com>
2021-05-06 23:37:56 -0500

    cocktails complete

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
+        praesentium consectetur at accusantium, quos eligendi quibusdam dicta
+        rerum eius neque eaque laborum, in delectus illum blanditiis provident
+        nemo nisi ipsam ut fuga, repellendus animi. Tenetur libero aliquam
+        officiis nesciunt voluptate.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
